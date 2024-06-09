03efebbc87a2d51bbea9a4b28c8213e3dbe5026d
Abhishek Prakash <abhishek.prakash@choicehotels.com>
2024-06-09 19:52:27 +0530

    Complete Jetpack Compose tutorial app

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +31,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArticleImage(
+                        title = stringResource(R.string.app_header),
+                        paragraph1 = stringResource(R.string.paragraph1),
+                        paragraph2 = stringResource(R.string.paragraph2)
                     )
                 }
             }
@@ -31,17 +47,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ArticleBody(
+    title: String,
+    paragraph1: String,
+    paragraph2: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
+    ) {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = paragraph1,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = paragraph2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun ArticleImage(
+    title: String,
+    paragraph1: String,
+    paragraph2: String,
+    modifier: Modifier = Modifier
+) {
+
+    ArticleBody(
+        title = title,
+        paragraph1 = paragraph1,
+        paragraph2 = paragraph2
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ComposeArticleTheme {
-        Greeting("Android")
-    }
+    ArticleImage(
+        title = stringResource(R.string.app_header),
+        paragraph1 = stringResource(R.string.paragraph1),
+        paragraph2 = stringResource(R.string.paragraph2)
+    )
 }
